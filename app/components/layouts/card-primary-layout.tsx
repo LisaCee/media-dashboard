83283ba994d0fd9682553d9a06a6879dae3be92a
LisaCee <lisaceedev@gmail.com>
2026-02-28 14:49:29 -0800

    Tweak layout to wrap

--- app/components/layouts/card-primary-layout.tsx
+++ app/components/layouts/card-primary-layout.tsx
@@ -1,3 +1,9 @@
-export function CardPrimaryLayout({children}) {
-    return <div className="grid grid-cols-1 gap-2 md:grid-cols-4 md:gap-8">{children}</div>;
+import type { ReactNode } from 'react';
+
+export function CardPrimaryLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="grid grid-cols-1 gap-2 justify-center items-center md:grid-cols-[repeat(auto-fill,minmax(300px,1fr))] md:gap-8">
+      {children}
+    </div>
+  );
 }
